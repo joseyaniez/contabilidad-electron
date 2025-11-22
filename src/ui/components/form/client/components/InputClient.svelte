9e602b585e75165b98e8c0f73c84c6f65bc98b1b
José Yáñez <joseyanez2298@gmail.com>
2025-11-22 16:34:44 -0500

    ui: vista de opciones en el ClientInput

--- src/ui/components/form/client/components/InputClient.svelte
+++ src/ui/components/form/client/components/InputClient.svelte
@@ -1,5 +1,28 @@
+<script lang="ts">
+  let clientText = $state("");
+  
+  function onKeyPress(event: KeyboardEvent) {
+    console.log("Key pressed:", event.key);
+    window.electronAPI.clients.find(clientText, clientText, clientText)
+      .then((result: any) => {
+        console.log("Search result:", result);
+      })
+      .catch((error: any) => {
+        console.error("Error during client search:", error);
+      });
+  }
+</script>
 
-<div class="flex flex-col flex-1">
+<div class="flex flex-col relative flex-1">
   <label class="font-bold pb-1" for="client-input">Cliente</label>
-  <input class="p-1 border-2 border-bacalao-secondary border-solid rounded-full"  type="text" id="client-input"/>
+  <input onkeydowncapture={onKeyPress} bind:value={clientText} class="px-4 py-1 relative z-20 bg-white  border-2 border-bacalao-secondary border-solid rounded-xl"  type="text" id="client-input"/>
+  <div class="absolute w-5/6 top-16 bg-white left-2">
+    <div class="p-2 border-x border-b rounded-b-xl border-bacalao-secondary flex flex-col">
+      <button onclick={() => {}} class="px-2 py-1 text-left rounded-md hover:bg-indigo-50">Ejemplo 1</button>
+      <button onclick={() => {}} class="px-2 py-1 text-left rounded-md hover:bg-indigo-50">Ejemplo 1</button>
+      <button onclick={() => {}} class="px-2 py-1 text-left rounded-md hover:bg-indigo-50">Ejemplo 1</button>
+      <button onclick={() => {}} class="px-2 py-1 text-left rounded-md hover:bg-indigo-50">Ejemplo 1</button>
+      <button onclick={() => {}} class="px-2 py-1 text-left rounded-md hover:bg-indigo-50">Ejemplo 1</button>
+    </div>
+  </div>
 </div>
